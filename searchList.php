af686eed8803aaed4a72ec2c2646e193a191a0d8
John Tunisi <John Tunisi>
2016-04-17 20:31:12 -0400

    Changed searchList back

--- searchList.php
+++ searchList.php
@@ -9,11 +9,10 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-$_SESSION['name']=$_GET['name'];
-    $query=mysqli_query($conn,"SELECT * FROM Lists WHERE name LIKE '%" . $_SESSION['name'] .  "%';");
+    $query=mysqli_query($conn,"SELECT * FROM Lists WHERE name LIKE '%" . $_GET['name'] .  "%';");
     $count=mysqli_num_rows($query);
     $rows= array();
-    while($r = mysqli_fetch_assoc($query)) {
+    while($r = mysqli_fetch_row($query)) {
       $rows[] = $r;
     }
     if ($count>0)
