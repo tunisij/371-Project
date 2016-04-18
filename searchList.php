d5766181386cf2333bf9beba859c2ef6ec369d20
John Tunisi <John Tunisi>
2016-04-17 20:24:42 -0400

    Changed searchList

--- searchList.php
+++ searchList.php
@@ -3,21 +3,19 @@ $servername = "cis.gvsu.edu";
 $username = "sinclair";
 $password = "sinclair5543";
 $dbname =  "sinclair";
-
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-
-    $query=mysqli_query($conn,"SELECT * FROM Lists WHERE name LIKE '%" . $_GET['name'] .  "%';");
+$_SESSION['name']=$_GET['name'];
+    $query=mysqli_query($conn,"SELECT * FROM Lists WHERE name LIKE '%" . $_SESSION['name'] .  "%';");
     $count=mysqli_num_rows($query);
     $rows= array();
-    while($r = mysqli_fetch_row($query)) {
+    while($r = mysqli_fetch_assoc($query)) {
       $rows[] = $r;
     }
-
     if ($count>0)
     {
       echo json_encode($rows);
@@ -26,7 +24,5 @@ if (!$conn) {
     {
       echo '{"success":false}';
     }
-
-
 $conn->close();
 ?>
